d470a95526658ff1d632b92e074ed9b256086006
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-09-06 10:46:31 +0200

    Add project files.

---
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Stl_Main.BackgroundColor = Color.LightGreen;
+        }
+
+        private void Btn_KlickMich_Clicked(object sender, EventArgs e)
+        {
+            Btn_KlickMich.Text = "Ich wurde angeklickt";
+        }
+    }
+}
